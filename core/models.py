51bb4b2516d99c12c90ece0bcb02084079d7e0e7
admin <admin@email>
2026-01-24 16:24:32 +0500

    update models

--- core/models.py
+++ core/models.py
@@ -3,16 +3,18 @@ CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
+    is_login BOOLEAN DEFAULT TRUE
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 """
 
-todos_table = """
+products = """
 CREATE TABLE IF NOT EXISTS todos (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
     title VARCHAR(255) NOT NULL,
-    status BOOLEAN DEFAULT FALSE,
+    price VARCHAR(255) NOT NULL
+    description VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 """
+
